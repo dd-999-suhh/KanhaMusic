562157990ca54e6f72f54a358d5904ae40684197
dd-999-suhh <julierbo791@gmail.com>
2026-09-11 19:29:10 +0630

    PANDA

--- config/config.go
+++ config/config.go
@@ -57,7 +57,7 @@ var (
 	SetCmds        = getEnvBool("SET_CMDS", false)
 	MaxAuthUsers   = getEnvInt("MAX_AUTH_USERS", 25)
 
-	StartImages = getEnvStrings("START_IMAGES","https://n.uguu.se/lEAsLEik.jpg")
+	StartImages = getEnvStrings("START_IMAGES")
 
 	PingImage = getEnv(
 		"PING_IMG_URL",
